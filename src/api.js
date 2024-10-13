a1c92e56915e41b41bbe79c713fab31e98fb4479
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-10-13 23:29:28 +0530

    update the fetch functions

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-export const getVans = async () => {
-  const response = await fetch('/api/vans');
+export const getVans = async (id) => {
+  const url = id ? `/api/vans/${id}` : '/api/vans';
+  const response = await fetch(url);
 
   if (!response.ok) {
     throw {
@@ -11,3 +12,18 @@ export const getVans = async () => {
   const data = await response.json();
   return data.vans;
 };
+
+
+export const getHostVans = async (id) => {
+  const url =  id ? `/api/host/vans/${id}` : "/api/host/vans"
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw {
+      message: 'Failed to fetch vans',
+      statusText: response.statusText,
+      status: response.status,
+    };
+  }
+  const data = await response.json();
+  return data.vans;
+}
