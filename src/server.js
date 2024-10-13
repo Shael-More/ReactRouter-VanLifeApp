5afd2f1c1aa4ab0a9d0d41e4618db804a6a81cdd
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-10-13 23:30:02 +0530

    update in server file

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import { createServer, Model } from 'miragejs';
+import { createServer, Model, Response } from 'miragejs';
 
 createServer({
   models: {
@@ -71,10 +71,10 @@ createServer({
   routes() {
     this.namespace = 'api';
     this.logging = false
-    this.timing = 2000
+    // this.timing = 2000
 
     this.get('/vans', (schema, request) => {
-      // return new Response(404, {}, {error: "Error Fetching Data"})
+      // return new Response(400, {}, {error: "Error Fetching Data"})
       return schema.vans.all();
     });
 
